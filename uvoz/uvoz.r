88f97ede3302cb359763eda88dbad943a653330a
Mirjam Pergar <mirjam.pergar@gmail.com>
2014-11-25 23:36:09 +0100

    Uvožene 3 tabele CSV. - REGIJE, STAROST2, SPOL

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -17,3 +17,44 @@ druzine <- uvoziDruzine()
 # datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
 # 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
 # fazah.
+
+uvoziSPOL <- function(){
+  return(read.table("podatki/SplZdrStSPOL.csv", sep = ";", as.is = TRUE,
+                    row.names = 1,
+                    col.names = c(" ",rep("2005",5),rep("2006",5),rep("2007",5),rep("2008",5),rep("2009",5),rep("2010",5),rep("2011",5),rep("2012",5),rep("2013",5)),
+                    fileEncoding = "Windows-1250"))
+  
+}
+
+cat("Uvažam podatke o splošnem zdr. stanju po SPOLU ...\n")
+SplZdrStSPOL <- uvoziSPOL()
+
+uvoziREGIJE <- function(){
+  return(read.table("podatki/SplZadZivljREGIJE.csv", sep = ";", as.is = TRUE,
+                    row.names = 1,
+                    fileEncoding = "Windows-1250"))
+  
+}
+
+cat("Uvažam podatke o splošnem zadovoljstvu z življ. po REGIJAH ...\n")
+SplZadZivljREGIJE <- uvoziREGIJE()
+
+# uvoziSTAROST1 <- function(){
+#   return(read.table("podatki/SplZadZivljSTAROST.csv", sep = ";", as.is = TRUE,
+#                     row.names = 1,
+#                     fileEncoding = "Windows-1250"))
+#   
+# }
+# 
+# cat("Uvažam podatke o splošnem zadovoljstvu z življ. po STAROSTI ...\n")
+# SplZadZivljSTAROST <- uvoziSTAROST1()
+
+uvoziSTAROST2 <- function(){
+  return(read.table("podatki/SplZdrStSTAROST.csv", sep = ";", as.is = TRUE,
+                    row.names = 1,
+                    fileEncoding = "Windows-1250"))
+  
+}
+
+cat("Uvažam podatke o splošnem zdravstvenem stanju glede na STAROST ...\n")
+SplZdrStSTAROST <- uvoziSTAROST2()

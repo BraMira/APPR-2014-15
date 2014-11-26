f7c80779a4afcd11c29c2a1f3d11a556cf0699b1
Mirjam Pergar <mirjam.pergar@gmail.com>
2014-11-26 21:19:49 +0100

    Uvozila sem vse CSV tabele in jih uredila.

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,91 +1,81 @@
 # 2. faza: Uvoz podatkov
 
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvoziDruzine <- function() {
-  return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
-                      row.names = 1,
-                      col.names = c("obcina", "en", "dva", "tri", "stiri"),
-                      fileEncoding = "Windows-1250"))
-}
-
-# Zapišimo podatke v razpredelnico druzine.
-cat("Uvažam podatke o družinah...\n")
-druzine <- uvoziDruzine()
-
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
-
-uvoziSPOL <- function(){
-  return(read.table("podatki/SplZdrStSPOL.csv", sep = ";", as.is = TRUE,
-                    row.names = 1,
-                    col.names = c(" ",rep("2005",5),rep("2006",5),rep("2007",5),rep("2008",5),rep("2009",5),rep("2010",5),rep("2011",5),rep("2012",5),rep("2013",5)),
-                    fileEncoding = "Windows-1250"))
-  
-}
-
-cat("Uvažam podatke o splošnem zdr. stanju po SPOLU ...\n")
-SplZdrStSPOL <- uvoziSPOL()
-
 uvoziREGIJE <- function(){
   return(read.table("podatki/SplZadZivljREGIJE.csv", sep = ";", as.is = TRUE,
-                    row.names = 1,
+                    
                     fileEncoding = "Windows-1250"))
   
 }
-
-cat("Uvažam podatke o splošnem zadovoljstvu z življ. po REGIJAH ...\n")
+cat("Uvažam podatke o splošnem zadovoljstvu z življenjem po REGIJAH ...\n")
 SplZadZivljREGIJE <- uvoziREGIJE()
 
-# uvoziSTAROST1 <- function(){
-#   return(read.table("podatki/SplZadZivljSTAROST.csv", sep = ";", as.is = TRUE,
-#                     row.names = 1,
-#                     fileEncoding = "Windows-1250"))
-#   
-# }
-# 
-# cat("Uvažam podatke o splošnem zadovoljstvu z življ. po STAROSTI ...\n")
-# SplZadZivljSTAROST <- uvoziSTAROST1()
+uvoziSTANJE <- function(){
+  return(read.table("podatki/ZadovoljstvoSTANJE1213.csv", sep = ";",as.is = TRUE, skip = 6,na.strings= "NA",
+                    col.names=c("Spol", "Zdravstveno stanje",paste0(procenti, "_2012"), paste0(procenti, "_2013")),
+                    fileEncoding = "Windows-1250"))
+}
+cat("Uvažam podatke o splošnem zadovoljstvu z življenjem glede na ZDRAVSTVENO STANJE ...\n")
+ZadovoljstvoSTANJE1213 <- uvoziSTANJE()
 
-uvoziSTAROST2 <- function(){
-  return(read.table("podatki/SplZdrStSTAROST.csv", sep = ";", as.is = TRUE,
-                    row.names = 1,
+procenti <- c("0-4 (%)","5-6 (%)","7-8 (%)","9-10 (%)","Neznano (%)","Povprečje")
+procenti[1:5]<-  c("Povsem nezadovoljen","Nezadovoljen", "Zadovoljen","Zelo zadovoljen", "Neznano")
+
+uvoziSTAROST1 <- function(){
+  return(read.table("podatki/ZadovoljstvoSTAROST1213.csv", sep = ";", skip= 6,as.is = TRUE, na.strings= "NA",
+                    col.names = c("Starosti","Spol", paste0(procenti, "_2012"), paste0(procenti, "_2013")),
                     fileEncoding = "Windows-1250"))
   
 }
 
-cat("Uvažam podatke o splošnem zdravstvenem stanju glede na STAROST ...\n")
+cat("Uvažam podatke o splošnem zadovoljstvu z življenjem glede na STAROSTI ...\n")
+ZadovoljstvoSTAROST1213 <- uvoziSTAROST1()
+
+uvoziSTAROST2 <- function(){
+  return(read.table("podatki/SplZdrStSTAROST.csv", sep = ";", as.is = TRUE,skip = 5,
+                    
+                    col.names = c("Starosti",paste0(stanja1,"_", 2005),paste0(stanja1, "_", 2006),paste0(stanja1, "_", 2007),paste0(stanja1, "_", 2008),paste0(stanja1, "_", 2009),paste0(stanja1, "_", 2010),paste0(stanja1, "_", 2011),paste0(stanja1, "_", 2012),paste0(stanja1, "_", 2013)),
+                    fileEncoding = "Windows-1250"))
+  
+}
+
+cat("Uvažam podatke o zdravstvenem stanju glede na STAROST ...\n")
 SplZdrStSTAROST <- uvoziSTAROST2()
 
+stanja <- c("Zelo slabo", "Slabo","Srednje","Dobro","Zelo dobro")
+stanja1 <- c("Zelo dobro","Dobro","Srednje","Slabo","Zelo slabo")
+stanja2 <- c("ZD","D","SR","S","ZS")
+
 uvoziSPOL1 <- function(){
-  return(read.table("podatki/skupajSPOL.csv", sep=";", as.is= TRUE, skip= 2,
-                      row.names = 1,
+  return(read.table("podatki/SPOL/skupajSPOL.csv", sep=";", as.is= TRUE, skip= 2,
+                    row.names = 1,
                       col.names = c("Stanje",paste0("S", 2005:2013)),fileEncoding = "Windows-1250"))
 }
 
-cat("Uvažam podatke o zdravstvenem stanju po SPOLU skupaj...\n")
+cat("Uvažam podatke o zdravstvenem stanju oseb glede na OBA SPOLA ...\n")
 skupajSPOL <- uvoziSPOL1()
 
 uvoziSPOL2 <- function(){
-  return(read.table("podatki/moskiSPOL.csv", sep=";", as.is= TRUE, skip= 2,
+  return(read.table("podatki/SPOL/moskiSPOL.csv", sep=";", as.is= TRUE, skip= 2,
                     row.names = 1,
                     col.names = c("Stanje",paste0("M", 2005:2013)),fileEncoding = "Windows-1250"))
 }
 
-cat("Uvažam podatke o zdravstvenem stanju po SPOLU moški...\n")
+cat("Uvažam podatke o zdravstvenem stanju MOŠKIH ...\n")
 moskiSPOL <- uvoziSPOL2()
 
 uvoziSPOL3 <- function(){
-  return(read.table("podatki/zenskeSPOL.csv", sep=";", as.is= TRUE, skip= 2,
-                    row.names = c("Zelo dobro","Dobro","Srednje","Slabo","Zelo slabo"),
+  return(read.table("podatki/SPOL/zenskeSPOL.csv", sep=";", as.is= TRUE, skip= 2,
+                    row.names = 1,
                     col.names = c("Stanje",paste0("Z", 2005:2013)),fileEncoding = "Windows-1250"))
 }
 
-cat("Uvažam podatke o zdravstvenem stanju po SPOLU ženski...\n")
+cat("Uvažam podatke o zdravstvenem stanju ŽENSK...\n")
 zenskeSPOL <- uvoziSPOL3()
-#zenskeSPOL <- zenskeSPOL
-# #Združimo vse spole
-#  SPOL1<- merge(moskiSPOL, zenskeSPOL, by=0 ,all = TRUE)
-# SPOL <- merge(SPOL1, skupajSPOL, by = 0, all = FALSE)
+
+
+#Združimo vse spole
+SPOL1<- merge(moskiSPOL, zenskeSPOL, by=0, all = TRUE)
+rownames(SPOL1)<- SPOL1$Row.names
+SPOL1 <- SPOL1[-1]
+SPOL <- merge(SPOL1, skupajSPOL, by = 0, all = TRUE)
+

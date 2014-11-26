4ab7cf3690b15c0f3b1541bddb7a082843101a6c
Mirjam Pergar <mirjam.pergar@gmail.com>
2014-11-26 21:20:08 +0100

    Poskušala sem uvesti HTML.

--- lib/xml.r
+++ lib/xml.r
@@ -2,15 +2,47 @@
 
 library(XML)
 
-# Vrne vektor nizov z odstranjenimi začetnimi in končnimi "prazninami" (whitespace)
-# iz vozlišč, ki ustrezajo podani poti.
+# # Vrne vektor nizov z odstranjenimi začetnimi in končnimi "prazninami" (whitespace)
+# # iz vozlišč, ki ustrezajo podani poti.
+# stripByPath <- function(x, path) {
+#   unlist(xpathApply(x, path,
+#                     function(y) gsub("^\\s*(.*?)\\s*$", "\\1", xmlValue(y))))
+# }
+# 
+# uvozi.obcine <- function() {
+#   url.obcine <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
+#   doc.obcine <- htmlTreeParse(url.obcine, useInternalNodes=TRUE)
+#   
+#   # Poiščemo vse tabele v dokumentu
+#   tabele <- getNodeSet(doc.obcine, "//table")
+#   
+#   # Iz druge tabele dobimo seznam vrstic (<tr>) neposredno pod
+#   # trenutnim vozliščem
+#   vrstice <- getNodeSet(tabele[[2]], "./tr")
+#   
+#   # Seznam vrstic pretvorimo v seznam (znakovnih) vektorjev
+#   # s porezanimi vsebinami celic (<td>) neposredno pod trenutnim vozliščem
+#   seznam <- lapply(vrstice[2:length(vrstice)], stripByPath, "./td")
+#   
+#   # Iz seznama vrstic naredimo matriko
+#   matrika <- matrix(unlist(seznam), nrow=length(seznam), byrow=TRUE)
+#   
+#   # Imena stolpcev matrike dobimo iz celic (<th>) glave (prve vrstice) prve tabele
+#   colnames(matrika) <- gsub("\n", " ", stripByPath(tabele[[2]][[1]], ".//th"))
+#   
+#   # Podatke iz matrike spravimo v razpredelnico
+#   return(data.frame(apply(gsub("\\*", "",
+#                           gsub(",", ".",
+#                           gsub("\\.", "", matrika[,2:5]))),
+#                     2, as.numeric), row.names=matrika[,1]))
+# }
 stripByPath <- function(x, path) {
   unlist(xpathApply(x, path,
                     function(y) gsub("^\\s*(.*?)\\s*$", "\\1", xmlValue(y))))
 }
 
 uvozi.obcine <- function() {
-  url.obcine <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
+  url.obcine <- "http://pxweb.stat.si/pxweb/Dialog/Saveshow.asp"
   doc.obcine <- htmlTreeParse(url.obcine, useInternalNodes=TRUE)
   
   # Poiščemo vse tabele v dokumentu
@@ -18,21 +50,26 @@ uvozi.obcine <- function() {
   
   # Iz druge tabele dobimo seznam vrstic (<tr>) neposredno pod
   # trenutnim vozliščem
-  vrstice <- getNodeSet(tabele[[2]], "./tr")
+  vrstice <- getNodeSet(tabele[[3]], "./tr")
   
   # Seznam vrstic pretvorimo v seznam (znakovnih) vektorjev
   # s porezanimi vsebinami celic (<td>) neposredno pod trenutnim vozliščem
-  seznam <- lapply(vrstice[2:length(vrstice)], stripByPath, "./td")
+  seznam <- lapply(vrstice, stripByPath, "./td/div")
   
   # Iz seznama vrstic naredimo matriko
   matrika <- matrix(unlist(seznam), nrow=length(seznam), byrow=TRUE)
   
   # Imena stolpcev matrike dobimo iz celic (<th>) glave (prve vrstice) prve tabele
-  colnames(matrika) <- gsub("\n", " ", stripByPath(tabele[[2]][[1]], ".//th"))
+  colnames(matrika) <- gsub("\n", " ", stripByPath(tabele[[4]], ".//th/div"))
+  rownames(matrike) <- gsub("\n"," ", stripByPath(tabele[[5]],".//th"))
   
   # Podatke iz matrike spravimo v razpredelnico
-  return(data.frame(apply(gsub("\\*", "",
-                          gsub(",", ".",
-                          gsub("\\.", "", matrika[,2:5]))),
-                    2, as.numeric), row.names=matrika[,1]))
-}
+  return(data.frame(apply(gsub("[,:]","",matrika),
+                          2, as.numeric))
+  )
+  
+}  
+ r <- data.frame(apply(gsub("[,:]","",matrika),
+                        2, as.numeric))
+View(r)
+ 

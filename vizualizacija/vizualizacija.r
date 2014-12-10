852fddb1bec94bec68330d22e887308b2a4c509f
Mirjam Pergar <mirjam.pergar@gmail.com>
2014-12-10 11:57:22 +0100

    Dodan zemljevid Slovenije :)

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -5,42 +5,42 @@ source("lib/uvozi.zemljevid.r")
 
 # Uvozimo zemljevid.
 cat("Uvažam zemljevid...\n")
-obcine <- uvozi.zemljevid("http://e-prostor.gov.si/fileadmin/BREZPLACNI_POD/RPE/OB.zip",
-                          "obcine", "OB/OB.shp", mapa = "zemljevid",
+regije <- uvozi.zemljevid("http://biogeo.ucdavis.edu/data/gadm2/shp/SVN_adm.zip",
+                          "Slovenija", "SVN_adm1.shp", mapa = "zemljevid",
                           encoding = "Windows-1250")
 
 # Funkcija, ki podatke preuredi glede na vrstni red v zemljevidu
-preuredi <- function(podatki, zemljevid) {
-  nove.obcine <- c()
-  manjkajo <- ! nove.obcine %in% rownames(podatki)
-  M <- as.data.frame(matrix(nrow=sum(manjkajo), ncol=length(podatki)))
-  names(M) <- names(podatki)
-  row.names(M) <- nove.obcine[manjkajo]
-  podatki <- rbind(podatki, M)
-  
-  out <- data.frame(podatki[order(rownames(podatki)), ])[rank(levels(zemljevid$OB_UIME)[rank(zemljevid$OB_UIME)]), ]
-  if (ncol(podatki) == 1) {
-    out <- data.frame(out)
-    names(out) <- names(podatki)
-    rownames(out) <- rownames(podatki)
-  }
-  return(out)
-}
+# preuredi <- function(podatki, zemljevid) {
+#   nove.obcine <- c()
+#   manjkajo <- ! nove.obcine %in% rownames(podatki)
+#   M <- as.data.frame(matrix(nrow=sum(manjkajo), ncol=length(podatki)))
+#   names(M) <- names(podatki)
+#   row.names(M) <- nove.obcine[manjkajo]
+#   podatki <- rbind(podatki, M)
+#   
+#   out <- data.frame(podatki[order(rownames(podatki)), ])[rank(levels(zemljevid$OB_UIME)[rank(zemljevid$OB_UIME)]), ]
+#   if (ncol(podatki) == 1) {
+#     out <- data.frame(out)
+#     names(out) <- names(podatki)
+#     rownames(out) <- rownames(podatki)
+#   }
+#   return(out)
+# }
 
 # Preuredimo podatke, da jih bomo lahko izrisali na zemljevid.
-druzine <- preuredi(druzine, obcine)
-
-# Izračunamo povprečno velikost družine.
-druzine$povprecje <- apply(druzine[1:4], 1, function(x) sum(x*(1:4))/sum(x))
-min.povprecje <- min(druzine$povprecje, na.rm=TRUE)
-max.povprecje <- max(druzine$povprecje, na.rm=TRUE)
-
-# Narišimo zemljevid v PDF.
-cat("Rišem zemljevid...\n")
-pdf("slike/povprecna_druzina.pdf", width=6, height=4)
-
-n = 100
-barve = topo.colors(n)[1+(n-1)*(druzine$povprecje-min.povprecje)/(max.povprecje-min.povprecje)]
-plot(obcine, col = barve)
+# druzine <- preuredi(druzine, obcine)
+# 
+# # Izračunamo povprečno velikost družine.
+# druzine$povprecje <- apply(druzine[1:4], 1, function(x) sum(x*(1:4))/sum(x))
+# min.povprecje <- min(druzine$povprecje, na.rm=TRUE)
+# max.povprecje <- max(druzine$povprecje, na.rm=TRUE)
+# 
+# # Narišimo zemljevid v PDF.
+# cat("Rišem zemljevid...\n")
+# pdf("slike/povprecna_druzina.pdf", width=6, height=4)
+# 
+# n = 100
+# barve = topo.colors(n)[1+(n-1)*(druzine$povprecje-min.povprecje)/(max.povprecje-min.povprecje)]
+# plot(obcine, col = barve)
 
 dev.off()
